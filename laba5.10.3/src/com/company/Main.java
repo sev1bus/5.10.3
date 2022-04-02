42f98128b45670ec51fb37fb851a696d36b3d1e3
sev1bus <101794447+sev1bus@users.noreply.github.com>
2022-04-02 15:42:39 +0300

    Add files via upload

---
+++ laba5.10.3/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int X = 1; X <= 10; X++) {
+            if (10 % X == 0) {
+                System.out.println("X = " + X + " - Дільник числа 10");
+            }
+        }
+    }
+}
